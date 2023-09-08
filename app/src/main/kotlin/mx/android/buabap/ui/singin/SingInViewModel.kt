56e507a9b5ab2ee55bddd94d6a3294d3f4240b30
salvador <salvadoredgar99@gmail.com>
2023-09-07 22:53:55 -0600

    Continue with view models

--- app/src/main/kotlin/mx/android/buabap/ui/singin/SingInViewModel.kt
+++ app/src/main/kotlin/mx/android/buabap/ui/singin/SingInViewModel.kt
@@ -1,13 +1,30 @@
 package mx.android.buabap.ui.singin
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import mx.android.buabap.domain.SignInUseCase
-import mx.android.buabap.ui.auth.AuthAction.SignIn
-import mx.android.buabap.ui.auth.AuthAction.SignUp
 
 class SingInViewModel(private val signInUseCase: SignInUseCase) : ViewModel() {
 
+    private val _signInUiState = MutableStateFlow<SignInUiState?>(null)
 
+    val signInUiState: StateFlow<SignInUiState?>
+        get() = _signInUiState
+
+    fun signIn(email: String, password: String) = viewModelScope.launch {
+        _signInUiState.value = SignInUiState.Loading
+
+        signInUseCase.signIn(email, password).collect { it ->
+            it.onSuccess {
+                _signInUiState.value = SignInUiState.Success
+            }
+            it.onFailure {
+                it.printStackTrace()
+                _signInUiState.value = SignInUiState.Error(it)
+            }
+        }
+    }
 }
--- app/src/main/kotlin/mx/android/buabap/ui/singup/SingUpViewModel.kt
+++ app/src/main/kotlin/mx/android/buabap/ui/singup/SingUpViewModel.kt
@@ -7,6 +7,13 @@ import mx.android.buabap.domain.SignInUseCase
 import mx.android.buabap.domain.SignUpUseCase
 import mx.android.buabap.ui.auth.AuthAction.SignIn
 import mx.android.buabap.ui.auth.AuthAction.SignUp
+import mx.android.buabap.ui.singin.SignInUiState
 
 class SingUpViewModel(private val signUpUseCase: SignUpUseCase) : ViewModel() {
+
+    private val _signUpUiState = MutableStateFlow<SignUpUiState?>(null)
+
+    val signUpUiState: StateFlow<SignUpUiState?>
+        get() = _signUpUiState
+
 }
