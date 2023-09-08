ca89e2710962bf9341a4dac65108e3820d77a8cc
salvador <salvadoredgar99@gmail.com>
2023-09-08 00:25:02 -0600

    Add delay

--- app/src/main/kotlin/mx/android/buabap/data/datasource/local/AuthLocalDataSource.kt
+++ app/src/main/kotlin/mx/android/buabap/data/datasource/local/AuthLocalDataSource.kt
@@ -1,5 +1,6 @@
 package mx.android.buabap.data.datasource.local
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import mx.android.buabap.data.datasource.exception.SignInException
@@ -11,16 +12,19 @@ import javax.inject.Inject
 class AuthLocalDataSource @Inject constructor(private val userDao: UserDao) {
 
     fun signUp(userEntity: UserEntity): Flow<Result<Boolean>> = flow {
+        delay(DELAY_RESPONSE)
         val id = userDao.insert(userEntity)
         if (id > INVALID_ID) emit(Result.success(true)) else emit(Result.failure(SignUpException()))
     }
 
     fun signIn(email: String, password: String): Flow<Result<UserEntity>> = flow {
+        delay(DELAY_RESPONSE)
         val userEntity = userDao.get(email, password)
         if (userEntity != null) emit(Result.success(userEntity)) else emit(Result.failure(SignInException()))
     }
 
     companion object {
         private const val INVALID_ID = -1L
+        private const val DELAY_RESPONSE = 500L
     }
 }
