6a7cea36edafd2404d96f8b18616144a00f614ef
salvador <salvadoredgar99@gmail.com>
2023-09-07 21:02:05 -0600

    Add empty DignUpViewModel and SingInViewModel.kt

---
+++ app/src/main/kotlin/mx/android/buabap/ui/singin/SingInViewModel.kt
@@ -0,0 +1,11 @@
+package mx.android.buabap.ui.singin
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import mx.android.buabap.domain.SignInUseCase
+import mx.android.buabap.ui.auth.AuthAction.SignIn
+import mx.android.buabap.ui.auth.AuthAction.SignUp
+
+class SingInViewModel(private val signInUseCase: SignInUseCase) : ViewModel() {
+}
---
+++ app/src/main/kotlin/mx/android/buabap/ui/singup/SingUpViewModel.kt
@@ -0,0 +1,11 @@
+package mx.android.buabap.ui.singup
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import mx.android.buabap.domain.SignInUseCase
+import mx.android.buabap.ui.auth.AuthAction.SignIn
+import mx.android.buabap.ui.auth.AuthAction.SignUp
+
+class SingUpViewModel(private val signUpViewModel: SingUpViewModel) : ViewModel() {
+}
