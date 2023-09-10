2024cacba3fdae73d40cffd93b1b30cdec89f6b7
salvador <salvadoredgar99@gmail.com>
2023-09-09 20:30:00 -0600

    Add other test to SingInActivity

--- app/src/androidTest/kotlin/mx/android/buabap/ui/singin/SignInActivityShould.kt
+++ app/src/androidTest/kotlin/mx/android/buabap/ui/singin/SignInActivityShould.kt
@@ -1,11 +1,22 @@
 package mx.android.buabap.ui.singin
 
+import android.content.Context
+import android.view.KeyEvent.KEYCODE_ENTER
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.pressKey
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import mx.android.buabap.ANY_INVALID_PASSWORD
+import mx.android.buabap.ANY_INVALID_USER_EMAIL
+import mx.android.buabap.ANY_USER_EMAIL
 import mx.android.buabap.R
+import mx.android.buabap.core.matcher.LoginErrorTextMatchers.withErrorText
 import org.junit.Test
 
 class SignInActivityShould {
@@ -19,4 +30,34 @@ class SignInActivityShould {
         onView(withId(R.id.password_edit_text)).check(matches(isDisplayed()))
         onView(withId(R.id.confirm_sign_in_button)).check(matches(isDisplayed()))
     }
+
+    @Test
+    fun displayErrorEmailInvalidWhenTypeInvalidEmail() {
+        launchActivity<SingInActivity>()
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val emailInvalid = context.getString(R.string.error_email_invalid)
+
+        onView(withId(R.id.email_edit_text)).perform(typeText(ANY_INVALID_USER_EMAIL), pressKey(KEYCODE_ENTER))
+        Espresso.closeSoftKeyboard()
+
+        onView(withId(R.id.confirm_sign_in_button)).perform(click())
+
+        onView(withId(R.id.email_input_layout)).check(matches(withErrorText(emailInvalid)))
+    }
+
+    @Test
+    fun displayErrorPasswordInvalidWhenTypeInvalidPassword() {
+        launchActivity<SingInActivity>()
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val passwordInvalid = context.getString(R.string.error_password_invalid)
+
+        onView(withId(R.id.email_edit_text)).perform(typeText(ANY_USER_EMAIL), pressKey(KEYCODE_ENTER))
+        onView(withId(R.id.password_edit_text)).perform(typeText(ANY_INVALID_PASSWORD), pressKey(KEYCODE_ENTER))
+        Espresso.closeSoftKeyboard()
+
+        onView(withId(R.id.confirm_sign_in_button)).perform(click())
+
+        onView(withId(R.id.password_input_layout)).check(matches(withErrorText(passwordInvalid)))
+    }
+
 }
